2260c9af5d14c0e1ef2df72ad215f9d292397460
e.herrera <ito2307@gmail.com>
2016-10-23 19:37:01 -0500

    Agregando mas ejemplos en javascript

---
+++ for-loop.js
@@ -0,0 +1,8 @@
+var total =0;
+var limit = 10;
+limit = 10-1;
+for(i=0;i<limit ;i++)
+{
+ total +=5;
+}
+console.log(total);
---
+++ if-statement.js
@@ -0,0 +1,6 @@
+var fruit = 'naranja';
+if (fruit.length > 5) {
+  console.log('The fruit name has more than five characters.');
+} else {
+  console.log('La fruta mencionada tiene igual o menos de cinco caracteres');
+}
